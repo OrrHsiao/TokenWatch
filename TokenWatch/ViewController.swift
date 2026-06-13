8187d10dd4e606147bb4eb13dc4d80aa0c620655
orrhsiao <orrhsiao@163.com>
2026-06-13 20:23:02 +0800

    feat: 初始化 TokenWatch macOS 工程

---
+++ TokenWatch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TokenWatch
+//
+//  Created by OrrHsiao on 2026/6/13.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ TokenWatchTests/TokenWatchTests.swift
@@ -0,0 +1,19 @@
+//
+//  TokenWatchTests.swift
+//  TokenWatchTests
+//
+//  Created by OrrHsiao on 2026/6/13.
+//
+
+import Testing
+@testable import TokenWatch
+
+struct TokenWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
